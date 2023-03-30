4db5e9bcc6b27dddb66b3b8c446408abeb66506d
apm29 <yjw999wow@163.com>
2023-03-30 19:02:24 +0800

    feat: 引入echarts

--- miniprogram/pages/charts/index.js
+++ miniprogram/pages/charts/index.js
@@ -1,66 +1,97 @@
 // pages/charts/index.js
+import * as echarts from '../../components/ec-canvas/echarts';
+const computedBehavior = require("miniprogram-computed").behavior
+const colorDict = {
+  1:"#35AA62",//支出
+  2:"#DEA106",//收入
+  3:"#1296db",//全部
+}
+
 Page({
 
+  behaviors:[computedBehavior],
   /**
    * 页面的初始数据
    */
   data: {
-
+    type: 1,
+    color: "#35AA62",
+    ec: {
+      lazyLoad: true
+    }
   },
+
+  // watch:{
+  //   type(type){
+  //     wx.setNavigationBarColor({
+  //       backgroundColor: colorDict[type],
+  //       frontColor: '#fff',
+  //     })
+  //   }
+  // },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log(getApp().globalData);
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  onReady(){
+    this.initCharts()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  initCharts(){
+     // 获取组件
+     const ecComponent = this.selectComponent('#echarts-container');
+     ecComponent.init((canvas, width, height, dpr) => {
+       console.log(arguments);
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // new
+      });
+      const option = {
+        backgroundColor: "transparent",
+        series: [{
+          label: {
+            normal: {
+              fontSize: 14
+            }
+          },
+          type: 'pie',
+          center: ['50%', '50%'],
+          radius: ['20%', '40%'],
+          data: [{
+            value: 55,
+            name: '北京'
+          }, {
+            value: 20,
+            name: '武汉'
+          }, {
+            value: 10,
+            name: '杭州'
+          }, {
+            value: 20,
+            name: '广州'
+          }, {
+            value: 38,
+            name: '上海'
+          }]
+        }]
+      };
+      chart.setOption(option);
+      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+      this.chart = chart;
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return chart;
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onUnload(){
+    this.charts?.dispose()
   }
+
 })
