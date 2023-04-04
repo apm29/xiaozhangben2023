470aa230cfd9d079d8ee94e9603e966eeaf041d0
apm29 <yjw999wow@163.com>
2023-04-04 15:20:36 +0800

    fix: 在expansion-panel组件中监听data变化,高度未超过maxHeight时隐藏

--- miniprogram/components/expansion-panel/index.js
+++ miniprogram/components/expansion-panel/index.js
@@ -1,16 +1,21 @@
 // components/expansion-panel/index.js
 const computedBehavior = require("miniprogram-computed").behavior;
+const defaultHeight = 500;
 Component({
   behaviors: [computedBehavior],
   /**
    * 组件的属性列表
    */
   properties: {
-    value:{
-      type:Boolean
+    value: {
+      type: Boolean
     },
-    maxHeight:{
-      type:Number
+    maxHeight: {
+      type: Number,
+      default: 500
+    },
+    data: {
+      type: [Array]
     }
   },
 
@@ -18,20 +23,26 @@ Component({
    * 组件的初始数据
    */
   data: {
-    internalValue: false
+    internalValue: false,
+    heightOverflow: true,
+    defaultHeight: defaultHeight,
   },
 
-  attached(){
+  attached() {
     this.setData({
       internalValue: Boolean(this.data.value)
     })
+    this.handleOverflow()
   },
 
-  watch:{
-    value(value){
+  watch: {
+    value(value) {
       this.setData({
         internalValue: Boolean(value)
-      })
+      });
+    },
+    data() {
+      this.handleOverflow()
     }
   },
 
@@ -39,13 +50,27 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    handleExpand(e){
+    handleExpand(e) {
       const expand = !this.data.internalValue
-      this.triggerEvent("input",expand)
+      this.triggerEvent("input", expand)
       this.setData({
         value: expand,
         internalValue: expand
       })
+    },
+    handleOverflow() {
+      const selector = this.createSelectorQuery()
+      selector.select(".expansion-content")
+        .boundingClientRect()
+        .exec(([node]) => {
+          function rpx2px(rpx) {
+            const { screenWidth } = wx.getSystemInfoSync()
+            return rpx / 750 * screenWidth
+          }
+          this.setData({
+            heightOverflow: node.height > rpx2px((this.data.maxHeight || defaultHeight))
+          })
+        })
     }
   }
 })
